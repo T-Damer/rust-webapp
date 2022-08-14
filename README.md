01b8c5d33a8382c9185351ecf07371ca9fad9779
Daniil <dpancov64@gmail.com>
2022-08-14 12:48:04 +0300

    production ready

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rust + Yew WebApp
+
+## How to run locally?
+
+1. `clone` this repo
+2. `cd` into directory
+3. Run `cargo install` to install `yew`
+4. Run `trunk serve` to launch server locally
+5. Open `localhost:8080` and enjoy :)
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use yew::prelude::*;
+
+struct Model {
+    value: i64,
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let state = use_state(|| Model { value: 0 });
+
+    let onclick = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value + 1,
+            })
+        })
+    };
+
+    html! {
+        <div>
+            <p>{state.value}</p>
+            <button {onclick}>{"Click me"}</button>
+        </div>
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
